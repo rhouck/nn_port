5acabcb57c3fbb6447fa3f7f2549aa65131a3c1c
Ryan Houck <ryan@levelskies.com>
2016-04-03 14:39:53 -0700

    added functionality to check kernel-based xs and ys are correct. other tests clean up

--- tests/gen_data.py
+++ tests/gen_data.py
@@ -16,10 +16,26 @@ def gen_random_onehot(date_index, width):
     df_maxs = df.apply(lambda x: list(x).index(max(x)), axis=1)
     return pd.get_dummies(df_maxs)
 
-def gen_2d_random_Xs_onehot_ys_from_random_kernel(date_index, width, classes):
-    true_weights = [np.random.randint(-5,6) for i in range(width)]
-    Xs = [gen_random_normal(date_index, len(true_weights)) for i in range(classes)]
+def gen_2d_random_Xs_onehot_ys_from_random_kernel(date_index, num_classes, num_features, noise_sigma=3.):
+    true_weights = [np.random.randint(-5,6) for i in range(num_features)]
+    Xs = [gen_random_normal(date_index, len(true_weights)) for i in range(num_classes)]
     ys = pd.DataFrame([(i * true_weights).sum(axis=1).values for i in Xs], columns=date_index).T
+    noise = pd.DataFrame(np.random.randn(ys.shape[0],ys.shape[1])*noise_sigma, columns=ys.columns, index=ys.index)
+    ys = ys + noise
     ys = pd.get_dummies(ys.apply(lambda x: list(x).index(max(x)), axis=1))
     Xs = pd.Panel(dict((ind, pd.DataFrame(i)) for ind, i in enumerate(Xs))).transpose(1,0,2)
-    return Xs, ys
+    return Xs, ys, true_weights
+
+def check_kernel_predictive_accuracy(Xs, ys, true_weights):
+    """apply kernel to Xs and return correct prediction accuracy"""
+    weights_df = pd.DataFrame(np.array([true_weights,]*Xs.shape[1]), index=Xs.major_axis, columns=Xs.minor_axis)
+    scaled = Xs.multiply(weights_df)
+    
+    def get_max_ind(df):
+        x = list(df.sum(axis=1).values)
+        return x.index(max(x))
+
+    df = pd.DataFrame({'xs': pd.Series([get_max_ind(scaled[item]) for item in scaled.items], index=ys.index),
+                       'ys': ys.apply(lambda x: list(x).index(max(x)), axis=1)})
+    matches = df.apply(lambda x: x[0]==x[1], axis=1)
+    return float(matches.sum()) / matches.shape[0]
--- tests/test_model.py
+++ tests/test_model.py
@@ -5,14 +5,32 @@ import numpy as np
 import pandas as pd
 
 import model_inputs as mi
-from gen_data import *
 import model as md
 import tensorflow as tf
+from gen_data import *
+
 
 class TestModelInputs(unittest.TestCase):
 
     def test_validation_doesnt_remove_or_distord_data_unnecessarilly(self):
         self.assertTrue(False)
+
+class TestDataGeneration(unittest.TestCase):
+    
+    def setUp(self):
+        np.random.seed(0)
+        self.dti = pd.DatetimeIndex(start='2000-1-1', freq='B', periods=1000)
+
+    def test_kernel_has_perfect_pred_power_with_no_noise(self):
+        Xs, ys, tw = gen_2d_random_Xs_onehot_ys_from_random_kernel(self.dti, 15, 10, 0.)
+        self.assertEquals(check_kernel_predictive_accuracy(Xs, ys, tw), 1.)
+
+    def test_adding_noise_reduces_kernel_predicted_power(self):    
+        accs = []
+        for i in (100., 10., 1., 0.):
+            Xs, ys, tw = gen_2d_random_Xs_onehot_ys_from_random_kernel(self.dti, 15, 10, i)
+            accs.append(check_kernel_predictive_accuracy(Xs, ys, tw))
+        self.assertEquals(sorted(accs), accs)
 
 class TestModel(unittest.TestCase):
     
@@ -22,12 +40,12 @@ class TestModel(unittest.TestCase):
         self.ys_probs = gen_random_probs(self.dti, 10)
         self.ys_labels = gen_random_onehot(self.dti, 10)
         Xs = gen_random_normal(self.dti, 20)
-        self.Xs, _ = validate_and_format_Xs_ys(Xs, self.ys_probs)
-        Xs_conv, ys_labels_conv = gen_2d_random_Xs_onehot_ys_from_random_kernel(self.dti, 5, 10)
-        self.Xs_conv, self.ys_labels_conv = validate_and_format_Xs_ys(Xs_conv, ys_labels_conv)
+        self.Xs, _ = mi.validate_and_format_Xs_ys(Xs, self.ys_probs)
+        Xs_conv, ys_labels_conv, _ = gen_2d_random_Xs_onehot_ys_from_random_kernel(self.dti, 5, 10)
+        self.Xs_conv, self.ys_labels_conv = mi.validate_and_format_Xs_ys(Xs_conv, ys_labels_conv)
 
     def test_input_output_are_same_shape(self):
-        inp = self.ys_labels.values
+        inp = self.ys_labels.values.astype(np.float32)
         probs, labels, _ = md.train_nn_softmax(inp, inp, [], 50, 10, .1)
         self.assertEquals(probs.shape, inp.shape)
         self.assertEquals(labels.shape[0], inp.shape[0])
@@ -43,25 +61,25 @@ class TestModel(unittest.TestCase):
             self.assertTrue(short_dist.all())
 
     def test_single_softmax_learns_onehot_w_perfect_foresight(self):
-        inp = self.ys_labels.values
+        inp = self.ys_labels.values.astype(np.float32)
         _, _, stats = md.train_nn_softmax(inp, inp, [], 500, 100, .1)
         self.assertTrue(stats['accuracy'] > .99)
 
     def test_hidden_layers_softmax_learns_onehot_w_perfect_foresight(self):
-        inp = self.ys_labels.values
+        inp = self.ys_labels.values.astype(np.float32)
         _, _, stats  = md.train_nn_softmax(inp, inp, [10,], 1000, 100, .1)
         self.assertTrue(stats['accuracy'] > .99)
 
     def test_single_softmax_learns_opt_weights_w_perfect_foresight(self):
         ys = pd.read_csv('tests/test_data/opt_weights_20.csv', index_col=0, parse_dates=['Date',])
-        probs, _, _ = md.train_nn_softmax(ys.values, ys.values, [], 2000, 1000, .4)
+        probs, _, _ = md.train_nn_softmax(ys.values.astype(np.float32), ys.values, [], 2000, 1000, .4)
         probs = pd.DataFrame(probs, columns=ys.columns, index=ys.index)
         self.assertTrue(probs.stack().corr(ys.stack()) > .95) 
 
     def test_noise_input_leads_to_stable_label_pred_based_on_max_val_freqs(self):
-        ys = pd.read_csv('tests/test_data/opt_weights_20.csv', index_col=0, parse_dates=['Date',])
-        Xs = gen_random_normal(ys.index, 20)
-        probs, _, _ = md.train_nn_softmax(Xs.values, ys.values, [], 1000, 100, .1)
+        ys = pd.read_csv('tests/test_data/opt_weights_20.csv', index_col=0, parse_dates=['Date',]).values
+        Xs = gen_random_normal(ys.index, 20).values.astype(np.float32)
+        probs, _, _ = md.train_nn_softmax(Xs, ys, [], 1000, 100, .1)
         probs = pd.DataFrame(probs, columns=ys.columns, index=ys.index)
         prob_ranks = probs.rank(axis=1).mean(axis=0).sort_values(ascending=False).index
         cols = list(ys.columns)
@@ -74,10 +92,10 @@ class TestModel(unittest.TestCase):
         self.assertTrue(rank_corr > .5)
         
     def test_regularization_decreases_ability_to_fit_train_set(self):
-        ys = self.ys_labels
+        ys = self.ys_labels.values.astype(np.float32)
         res = []
         for penalty_alpha in (0., .1, .5):
-            _, _, stats = md.train_nn_softmax(ys.values, ys.values, [2], 1000, 500, .1, penalty_alpha=penalty_alpha)
+            _, _, stats = md.train_nn_softmax(ys, ys, [2], 1000, 500, .1, penalty_alpha=penalty_alpha)
             res.append(1. - stats['accuracy'])
         self.assertTrue(all(res[i] <= res[i+1] for i in xrange(len(res)-1)))
 
@@ -107,7 +125,6 @@ class TestModel(unittest.TestCase):
     #     Xs = mi.flatten_panel(self.Xs_conv)
     #     _, _, stats = md.train_nn_softmax(Xs.values, ys.values, [2], 1000, 100, .1)
     #     print stats
-
 
 
 
