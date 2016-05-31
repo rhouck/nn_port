bbce83f0c38d768940c3c2ed8e7506b751f6c482
Ryan Houck <ryan@levelskies.com>
2016-05-31 17:16:15 +0100

    added optiont to penalize stock tilts

--- custom_objectives.py
+++ custom_objectives.py
@@ -19,10 +19,11 @@ def calc_gearing(logits, y, y_, returns_):
     abs_holdings = tf.abs(y) / 2.
     return tf.reduce_sum(abs_holdings, 1)
     
-def sigmoid_ir(logits, y, y_, returns_, activation, return_per_days, gain, holdings_gain, gearing_alpha, mean, pos):
+def sigmoid_ir(logits, y, y_, returns_, activation, return_per_days, gain, holdings_gain, gearing_alpha, mean, pos, pen_tilt):
     with tf.name_scope('sigmoid_ir'):
 
         net_holdings = calc_net_holdings(logits, y, y_, returns_)
+        #demeaned_holdings = tf.sub(y, tf.expand_dims(net_holdings, 1))
         
         if mean and pos:        
             # get average positive net holdings
@@ -41,11 +42,22 @@ def sigmoid_ir(logits, y, y_, returns_, activation, return_per_days, gain, holdi
         holdings_lim_1 = max_net_holdings / (tf.abs(max_net_holdings) + holdings_gain)
         holdings_penalty = 1. - holdings_lim_1
         
+        if pen_tilt:
+            tilts = tf.reduce_mean(y, 0)
+            if mean:
+                max_tilt = tf.reduce_mean(tf.abs(tilts))
+            else:
+                max_tilt = tf.reduce_max(tf.abs(tilts))
+            tilt_lim_1 = max_tilt / (tf.abs(max_tilt) + holdings_gain)
+            tilt_penalty = 1. - tilt_lim_1
+
+        #ir = calc_batch_ir(logits, demeaned_holdings, y_, returns_, return_per_days)
         ir = calc_batch_ir(logits, y, y_, returns_, return_per_days)
-        
         ir_scaled = ir * holdings_penalty
+        if pen_tilt:
+            ir_scaled *= tilt_penalty
         sigmoid_ir = tf.sigmoid(-gain * ir_scaled)
-        
+
         gearing = calc_gearing(logits, y, y_, returns_)
         avg_gearing = tf.reduce_mean(gearing)
         gearing_diff = tf.square(1. - avg_gearing) * gearing_alpha
