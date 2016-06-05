2af42ea0bcb1ea392653073deb2ff8bc92146d42
Ryan Houck <ryan@levelskies.com>
2016-06-05 19:03:49 +0100

    cleaned up sigmod ir calc and and updated gearing penalty to combine tilt and holdings values

--- custom_objectives.py
+++ custom_objectives.py
@@ -19,29 +19,28 @@ def calc_gearing(logits, y, y_, returns_):
     abs_holdings = tf.abs(y) / 2.
     return tf.reduce_sum(abs_holdings, 1)
 
-def calc_discount(metric, mean, gain):
+def calc_summary(metric, mean):
     if mean:
-        sel_metric = tf.reduce_mean(metric)
-    else:
-        sel_metric = tf.reduce_max(metric)
-    #lim_1 = tf.tanh(sel_metric)
-    lim_1 = sel_metric / (tf.abs(sel_metric) + gain)
+        return  tf.reduce_mean(metric)
+    return tf.reduce_max(metric)
+
+def calc_discount(metric, gain):
+    lim_1 = metric / (tf.abs(metric) + gain)
     return 1. - lim_1
     
 def sigmoid_ir(logits, y, y_, returns_, activation, return_per_days, sigmoid_gain, holdings_gain, gearing_gain, tilt_gain, mean):
     with tf.name_scope('sigmoid_ir'):
-
-        net_holdings = calc_net_holdings(logits, y, y_, returns_)
+        net_holdings = tf.abs(calc_net_holdings(logits, y, y_, returns_))
         gearing = calc_gearing(logits, y, y_, returns_)
-        #gearing_error = tf.abs(1. - gearing)
-        gearing_ratio = tf.abs(net_holdings) / gearing
         tilts = tf.abs(tf.reduce_mean(y, 0))
-
-        net_holdings_discount = calc_discount(net_holdings, mean, holdings_gain)
-        #gearing_discount = calc_discount(gearing_error, mean, gearing_gain)  
-        gearing_discount = calc_discount(gearing_ratio, mean, gearing_gain)
-        tilt_discount = calc_discount(tilts, mean, tilt_gain)
         
+        inp = [net_holdings, gearing, tilts]
+        net_holdings, gearing, tilts = map(lambda x: calc_summary(x, mean), inp)
+        gearing_ratio = tf.maximum(net_holdings, tilts) / gearing
+        inp = ((net_holdings, holdings_gain), (tilts, tilt_gain), (gearing_ratio, gearing_gain))
+        discounts = map(lambda x: calc_discount(x[0], x[1]), inp)
+        discounts = reduce(lambda x, y: x * y, discounts)
+
         ir = calc_batch_ir(logits, y, y_, returns_, return_per_days)
-        ir_scaled = ir * net_holdings_discount * gearing_discount * tilt_discount
+        ir_scaled = ir * discounts
         return tf.sigmoid(sigmoid_gain * -ir_scaled)
